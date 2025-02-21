db15d092ee4a7de75e16e144c012673396749281
tfc <kloczdupy228@gmail.com>
2025-02-21 17:10:23 +0100

    animals added

--- main.py
+++ main.py
@@ -36,6 +36,30 @@ def get_plants_data() -> list:
 
     return plants_cleaned_up_data
 
+# Returns list formatted [[animal_name, creation_day_quantity, current_day_quantity],...].
+def get_animals_data() -> list:
+    ANIMALS_DIR_SUFFIX = 'Server/Mods/__core__/AutoGen/Animal'
+
+    server_days_running = get_server_days_running()
+
+    animals_list = [animal.replace('.cs', '') for animal in
+                   os.listdir(os.path.join(GAME_DATA_LOCATION, ANIMALS_DIR_SUFFIX))]
+
+    animals_data = []
+
+    for animal in animals_list:
+        response = requests.get(os.path.join(SERVER_URL,
+                                  f"datasets/get?dataSet={animal}Species&dayStart=0&dayEnd={server_days_running}"))
+        animals_data.append([animal, response.json()])
+
+    animals_cleaned_up_data = []
+
+    for animal in animals_data:
+        cleaned_up_data = [animal[0], animal[1]['Values'][0], animal[1]['Values'][-1]]
+        animals_cleaned_up_data.append(cleaned_up_data)
+
+    return animals_cleaned_up_data
+
 def plants_data_visualization():
     plants_data = get_plants_data()
     name_width = max(len(plant[0]) for plant in plants_data) + 2
@@ -48,12 +72,26 @@ def plants_data_visualization():
             colored(" Name: ", "yellow").ljust(10) + colored(plant[0].ljust(name_width), "white") +
             colored(" Creation day quantity: ", "yellow").ljust(25) + colored(str(round(plant[1],1)).rjust(qty_width), "white") +
             colored(" Current day quantity: ", "yellow").ljust(25) + colored(str(round(plant[2],1)).rjust(qty_width), "white") +
-            colored(" Bilans: ", "green" if balance > 0 else "red").ljust(25) + colored(str(balance).rjust(qty_width), "green" if balance > 0 else "red")
+            colored(" Balance: ", "green" if balance > 0 else "red").ljust(25) + colored(str(balance).rjust(qty_width), "green" if balance > 0 else "red")
         )
 
+def animals_data_visualization():
+    animals_data = get_animals_data()
+    name_width = max(len(animal[0]) for animal in animals_data) + 2
+    qty_width = 10
+
+    for animal in animals_data:
+        balance = round(animal[2]-animal[1],1)
+
+        print(
+            colored(" Name: ", "yellow").ljust(10) + colored(animal[0].ljust(name_width), "white") +
+            colored(" Creation day quantity: ", "yellow").ljust(25) + colored(str(round(animal[1],1)).rjust(qty_width), "white") +
+            colored(" Current day quantity: ", "yellow").ljust(25) + colored(str(round(animal[2],1)).rjust(qty_width), "white") +
+            colored(" Balance: ", "green" if balance > 0 else "red").ljust(25) + colored(str(balance).rjust(qty_width), "green" if balance > 0 else "red")
+        )
 
 def main():
-    pass
+    animals_data_visualization()
 
 if __name__ == "__main__":
     main()
